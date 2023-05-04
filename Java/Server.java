256d661ff0cf2d2db3839f2385a37a82a21f7517
henry-bao <henrybao@uw.edu>
2023-05-03 17:09:59 -0700

    remove extra spacing

--- Java/Server.java
+++ Java/Server.java
@@ -42,10 +42,6 @@ public class Server {
         }
     }
 
-
-
-
-
     // Do not modify any code below tihs line
     // --------------------------------------
     public static String echo(String message) { 
